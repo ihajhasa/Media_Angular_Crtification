dab9719cc91eb7175a6fa0a96fd7b044672de43c
Ishaq Yousef <TCS@Ishaqs-MacBook-Pro.local>
2021-05-19 11:33:24 -0400

    Linked Bootstrap CDN + Jquery, + Defined routes to test components

--- Media/src/app/app-routing.module.ts
+++ Media/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'contactus',
+    component: ContactUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Media/src/app/components/register/register.component.ts
+++ Media/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  regForm: FormGroup = this.formBuilder.group({});
+  submitted: Boolean = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  onSubmit() {
+    this.submitted = true;
+    if (!this.regForm.valid) return;
+
+    alert("Not Yet Implemented");
+  }
+
+  get controls() {return this.regForm.controls}
 
   ngOnInit(): void {
+    this.regForm = this.formBuilder.group({
+      name: ["", [Validators.required]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required]]
+    })
   }
 
 }
