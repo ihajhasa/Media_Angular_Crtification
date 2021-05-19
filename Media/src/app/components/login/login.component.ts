88f7501458ab2a922dfbc87799b54b688ddfd441
mickey2721 <michaelabay16@gmail.com>
2021-05-19 09:29:54 -0700

    init

--- Media/src/app/components/login/login.component.ts
+++ Media/src/app/components/login/login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  Email:string;
+  Password:string;
   constructor() { }
 
   ngOnInit(): void {
