822308406e701b582cfa36db517d8cd4bc52ad58
Ishaq Yousef <TCS@Ishaqs-MacBook-Pro.local>
2021-05-19 13:20:10 -0400

    Edit News Component (NO Submit)

--- Media/src/app/components/edit-news/edit-news.component.ts
+++ Media/src/app/components/edit-news/edit-news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-news',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditNewsComponent implements OnInit {
 
-  constructor() { }
+  editNewsForm: FormGroup = this.formBuilder.group({})
+  submitted: Boolean = false;
+
+  //URL_REGEX: RegExp = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (!this.editNewsForm.valid) return;
+
+    alert("Not Yet Implemented\n\n" + JSON.stringify(this.editNewsForm.value, null, 4))
+  }
+  
+  get controls() {return this.editNewsForm.controls}
 
   ngOnInit(): void {
+    this.editNewsForm = this.formBuilder.group({
+      title: ["", [Validators.required]],
+      description: ["", [Validators.required]],
+      category: ["", [Validators.required]],
+      url: ["", [Validators.required]],
+      imgUrl: ["", [Validators.required]],
+      publishedDate: [null, Validators.required]
+    })
   }
 
 }
